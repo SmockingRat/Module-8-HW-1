75e03383a435d8e94f80e2e48b9c3a71c48c188c
SmokingRat <halfbrikbarry@gmail.com>
2022-03-27 19:41:37 +0300

    Добавьте файлы проекта.

---
+++ Module 8 HW-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_8_HW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
